22f8acea536237e6589b5177bc3675a9535bb3ff
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-10-29 21:02:37 -0500

    Return fibonacci with iteration

---
+++ recursion/fibonacci.js
@@ -0,0 +1,28 @@
+// N is the num of fibonacci numbers
+const fibs = (n) => {
+    let arr = [];
+
+    for(let i = 0; i < n; i++)
+    {
+        if(i == 0)
+        {
+            arr.push(0);
+        }
+        else if(i == 1)
+        {
+            arr.push(1);
+        }
+        else if(i == 2)
+        {
+            arr.push(1);
+        }
+        else
+        {     
+            arr.push(arr[i - 1] + arr[i - 2]);
+        }
+    }
+    return arr;
+}
+
+console.log(fibs(8));
+
