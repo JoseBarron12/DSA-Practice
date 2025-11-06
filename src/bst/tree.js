e1507060ba24a0fe66b480c21ecb4225c9f96ff1
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-11-06 02:50:26 -0600

    Implement BST insert + deletion

--- src/bst/tree.js
+++ src/bst/tree.js
@@ -47,13 +47,96 @@ export class BST {
         prettyPrint(this.root);
     }
 
+    insert(value, node)
+    {
+        if(node == null)
+        {
+            return new BSTNode(value, null, null)
+        }
+
+        if(value < node.root) 
+        {
+           node.left = this.insert(value, node.left)
+        }
+        else 
+        {
+            node.right = this.insert(value, node.right);
+        }
+        return node;
+    }
+
+    delete(value, node)
+    {
+        if(node.root == value)
+        {
+            if(node.left == null && node.right == null) // CASE 1: Desired Node Has No Children
+            {
+                return null;
+            }
+            else if(node.left != null && node.right != null) // CASE 2: DESIRED Node Has Two Children
+            {
+                let temp = node.right; // Search right subtree
+                
+                while(temp.left != null) // Find smallest value on right subtree (inorder successor)
+                {
+                    temp = temp.left;
+                }
+                
+                // Replace desired Value w/ its inorder successor
+                const toDeleteValue = node.root;
+                node.root = temp.root; 
+                temp.root = toDeleteValue;
+
+                node.right = this.delete(temp.root, node.right); // Traverse right tree again and delete replace node
+                return node;
+
+            }
+            // CASE 3: Desired Node has only One Child
+            else if(node.left != null) // CASE 3.1: Desired Node has only left Child
+            {
+                return node.left;
+            }
+            else // CASE 3.2: Desired Node has only Right Child
+            {
+                return node.right;
+            }
+
+        }
+
+        if(value < node.root) 
+        {
+           node.left = this.delete(value, node.left)
+        }
+        else 
+        {
+            node.right = this.delete(value, node.right);
+        }
+        return node;
+    }
+
 }
 
-const BSTree = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-BSTree.print();
+//const BSTree = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+//BSTree.print();
 
-const BSTree2 = new BST([10,20,30]);
-BSTree2.print();
+//const BSTree2 = new BST([10,20,30]);
+//BSTree2.print();
 
 const BSTree3= new BST([1, 5, 9, 14, 23, 27]);
 BSTree3.print();
+
+BSTree3.insert(10, BSTree3.root);
+BSTree3.print();
+
+BSTree3.insert(28, BSTree3.root);
+BSTree3.print();
+
+BSTree3.delete(28, BSTree3.root); // DELETE NODE W/O CHILDREN [LEAF NODE]
+BSTree3.print();
+
+BSTree3.delete(14, BSTree3.root); // DELETE NODE W/ ONE CHILD 
+BSTree3.print();
+
+BSTree3.delete(9, BSTree3.root); // DELETE NODE W/ TWO CHILDREN
+BSTree3.print();
+
